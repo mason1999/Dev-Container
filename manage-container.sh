a0a36bcbec23d2242110b7d8e28d85e793993064
mason wong <masonwong1999@gmail.com>
2023-12-21 16:05:18 +1100

    updated to add auto folder creation/deletion and PROJECT_NAME variable

--- manage-container.sh
+++ manage-container.sh
@@ -2,21 +2,23 @@
 
 OPTERR=0
 
-IMAGE_NAME="test_image"
-CONTAINER_NAME="test_container"
-MAPPED_FOLDER_NAME="dev_folder_1"
+PROJECT_NAME="test"
+IMAGE_NAME="${PROJECT_NAME}_image"
+CONTAINER_NAME="${PROJECT_NAME}_container"
+MAPPED_FOLDER_NAME="${PROJECT_NAME}_Desktop"
+HOST_FOLDER_PATH="${HOME}/Desktop/containers/${MAPPED_FOLDER_NAME}"
 HOST_PORT=2003
 CONTAINER_PORT=2003
 
 create() {
     # Create a file system in the containers folder which we build
-    mkdir -p "${HOME}/Desktop/containers/${MAPPED_FOLDER_NAME}"
+    mkdir -p "${HOST_FOLDER_PATH}"
     # Build Docker image
     docker build -t "${IMAGE_NAME}" .
     # Run image as container
     docker run -d -it -p "${HOST_PORT}:${CONTAINER_PORT}" -e DISPLAY \
         -v /tmp/.X11-unix:/tmp/.X11-unix \
-        -v "${HOME}/Desktop/containers/${MAPPED_FOLDER_NAME}:/home/testuser1/Desktop/${MAPPED_FOLDER_NAME}" \
+        -v "${HOST_FOLDER_PATH}:/home/testuser1/${MAPPED_FOLDER_NAME}" \
         --name "${CONTAINER_NAME}" "${IMAGE_NAME}"
 }
 
@@ -27,24 +29,19 @@ delete() {
     docker rm "${CONTAINER_NAME}"
     # Remove the image
     docker rmi "${IMAGE_NAME}"
+    # Remove the host folder
+    rm -rf "${HOLD_FOLDER_PATH}"
 }
 
-#################### BEGIN SCRIPT ###################
-getopts "cdh" option
-case $option in
-    c)
-    create ;;
-
-    d)
-    delete ;;
-
-    h)
-    ports_in_use=$(ss -tulpn | awk '{print $5}' | awk 'NR > 1' | sed 's/.*://' | sort -un | tr '\n' ',' | sed 's/,$/\n/')
-    cat <<EOF
-When using this script remember:
+help() {
+ports_in_use=$(ss -tulpn | awk '{print $5}' | awk 'NR > 1' | sed 's/.*://' | sort -un | tr '\n' ',' | sed 's/,$/\n/')
+cat <<EOF
+When using this script remember to change the internal variables inside the script:
+- PROJECT_NAME: The overarching project or idea. This will be used as an identifier for the image, container and mounted filesystem.
 - IMAGE_NAME: The name of the image to be built.
 - CONTAINER_NAME: The name of the container that will be built.
 - MAPPED_FOLDER_NAME: The name of the folder that will be created and mapped from the host to the container. Think of this as your persistent "dev" environment.
+- HOST_FOLDER_PATH: The path of the mapped filesystem from the host side
 - HOST_PORT: The port that will be used from the host side. 
 - CONTAINER_PORT: The port that will be used from the container side.
 
@@ -58,6 +55,22 @@ The available options are [-c|-d|-h] are:
     -d: Stopping and removing images and containers 
     -h: Getting help for the script
 EOF
+
+}
+
+#################### BEGIN SCRIPT ###################
+getopts "cdh" option
+case $option in
+    c)
+    create
+    ;;
+
+    d)
+    delete
+    ;;
+
+    h)
+    help
     ;;
 
     ?)
